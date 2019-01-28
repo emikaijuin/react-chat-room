0c860a4309a4107aad0a866be376f47783c249f3
Emika Ijuin <emikaijuin@gmail.com>
2019-01-28 16:51:43 +0800

    Update state to include most recently sent message and pass to Messages conainer via props

--- src/pages/ChatRoom.js
+++ src/pages/ChatRoom.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
 import MessageForm from "../containers/MessageForm"
+import Messages from "../containers/Messages"
 
 class ChatRoom extends Component {
-  state = {}
+  state = {
+    messages: []
+  }
+
+  appendMessage = message => {
+    var messages = [...this.state.messages]
+    messages.push(message)
+    this.setState({ messages: messages })
+  }
+
 
   render() {
     return (
       <div>
         I'm a chatroom
-        <MessageForm />
+        <Messages messages={this.state.messages} />
+        <MessageForm appendMessage={ this.appendMessage } />
       </div>
     )
   }
