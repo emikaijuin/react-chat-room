33cc4810ab05b91da65e909de94d0262069fbc83
Emika Ijuin <emikaijuin@gmail.com>
2019-01-28 17:58:39 +0800

    add basic layout and styling

--- src/containers/MessageForm.js
+++ src/containers/MessageForm.js
@@ -15,7 +15,8 @@ class MessageForm extends Component {
     if (this.messageIsSubmittable()) { 
       this.props.appendMessage({
         content: this.state.message,
-        username: "emikaijuin" // hardcoding to be replaced later
+        username: "emikaijuin", // hardcoding to be replaced later
+        timestamp: Date.now()
       }) 
       this.setState({message: ''})
     }
@@ -30,12 +31,25 @@ class MessageForm extends Component {
     return (
       <form onSubmit={ this.sendMessage }>
         <div>  
-          <input 
-            type = "text" 
+          <textarea 
             onChange = { this.handleMessageInput}
             value = { this.state.message }
+            style = {{
+              position: "absolute",
+              height: "15%",
+              width: "80%",
+              boxSizing: "border-box",
+              border: "0",
+              outline: "none"
+            }}
           />
-          <span>{ this.messageIsSubmittable() ? "" : "Max length is 500 characters"}</span>
+          <span style={{
+            position: "absolute",
+            bottom: "2.5%",
+            color: "red"
+          }}>
+            { this.messageIsSubmittable() ? "" : "Max length is 500 characters"}
+          </span>
         </div>
       </form>
     )
--- src/pages/ChatRoom.js
+++ src/pages/ChatRoom.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import MessageForm from "../containers/MessageForm"
 import Messages from "../containers/Messages"
+import Conversations from "../containers/Conversations"
 
 class ChatRoom extends Component {
   state = {
-    messages: []
+    messages: [],
+    conversationId: 0
   }
 
   appendMessage = message => {
@@ -13,13 +15,31 @@ class ChatRoom extends Component {
     this.setState({ messages: messages })
   }
 
+  selectActiveConversation = conversationId => {
+    this.setState({activeConversation: conversationId})
+  }
+
 
   render() {
     return (
-      <div>
-        I'm a chatroom
-        <Messages messages={this.state.messages} />
-        <MessageForm appendMessage={ this.appendMessage } />
+      <div className="chat-room" >
+        <div className="conversations-container">
+          <Conversations 
+            selectActiveConversation = { this.selectActiveConversation } 
+          />
+        </div>
+        <div className="messages-container">
+          <div className="messages-content">
+            <Messages 
+              messages = {this.state.messages} 
+              />
+          </div>
+          <div className="message-form">
+            <MessageForm 
+              appendMessage = { this.appendMessage } 
+            />
+          </div>
+        </div>
       </div>
     )
   }
