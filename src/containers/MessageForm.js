35d5133ac7ca4dc833c8d37aad0ca633c7dc4133
Emika Ijuin <emikaijuin@gmail.com>
2019-01-28 16:27:34 +0800

    message is only submitted if length validation is met

---
+++ src/containers/MessageForm.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react"
+
+class MessageForm extends Component {
+  state = {
+    message: '',
+    error: ''
+  }
+
+  handleMessageInput = event => {
+    this.setState({ message: event.target.value })
+  }
+
+  handleFormSubmit = event => {
+    event.preventDefault()
+    if (this.messageIsSubmittable()) { this.props.appendMessage() }
+  }
+
+  messageIsSubmittable = () => (
+    this.state.message.length < 5 ? true : false
+  )
+
+
+  render() {
+    return (
+      <form onSubmit={ this.handleFormSubmit }>
+        <div>  
+          <input 
+            type = "text" 
+            onChange = { this.handleMessageInput}
+            value = { this.state.message }
+          />
+          <span>{ this.messageIsSubmittable() ? "" : "Max length is 500 characters"}</span>
+        </div>
+      </form>
+    )
+  }
+}
+
+export default MessageForm
