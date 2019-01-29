e4e32a7676e85b2e4ab47e052271406052cf14e0
Emika Ijuin <emikaijuin@gmail.com>
2019-01-29 10:45:34 +0800

    form uses textarea for message input to allow for multiline typing. additional event listener is added to check if user hits enter (without shift key), which will trigger the message to be sent

--- src/containers/MessageForm.js
+++ src/containers/MessageForm.js
@@ -6,6 +6,12 @@ class MessageForm extends Component {
     error: ''
   }
 
+  checkForSubmit = event => {
+    if (event.keyCode === 13 && !event.shiftKey) {
+      this.sendMessage(event)
+    }
+  }
+
   handleMessageInput = event => {
     this.setState({ message: event.target.value })
   }
@@ -29,28 +35,41 @@ class MessageForm extends Component {
 
   render() {
     return (
-      <form onSubmit={ this.sendMessage }>
-        <div>  
-          <textarea 
-            onChange = { this.handleMessageInput}
+      <form onSubmit = { this.sendMessage } >
+        {/* <label> */}
+          {/* Message:  */}
+          <textarea
+            type = "text"
+            onKeyDown = { this.checkForSubmit } // text areas don't submit form on enter, so additional event listener is added on key down to check if user hit enter iwthout shift
+            onChange = { this.handleMessageInput} 
             value = { this.state.message }
             style = {{
               position: "absolute",
               height: "15%",
-              width: "80%",
+              width: "75%",
               boxSizing: "border-box",
               border: "0",
               outline: "none"
             }}
           />
-          <span style={{
+        {/* </label> */}
+        <input 
+          type="submit"
+          className="btn btn-outline-info"
+          style={{
             position: "absolute",
-            bottom: "2.5%",
-            color: "red"
-          }}>
-            { this.messageIsSubmittable() ? "" : "Max length is 500 characters"}
-          </span>
-        </div>
+            right: "0",
+            bottom: "10%",
+            transform: "translate(-25%, 50%)"
+          }}
+          />
+        <span style={{
+          position: "absolute",
+          bottom: "2.5%",
+          color: "red"
+        }}>
+          { this.messageIsSubmittable() ? "" : "Max length is 500 characters"}
+        </span>
       </form>
     )
   }
